26cc2d599f02e4e7ece000227a071cec3799695f
dytroInc <dytrocodes@gmail.com>
2021-02-14 18:49:20 +0900

    website update 2

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import NotFound from 'components/NotFound';
-import Home from 'containers/Home';
 import Workshop from 'containers/Workshop';
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
@@ -14,7 +13,6 @@ export default function App() {
       <Switch style={{
         height: '100%'
       }}>
-        <Route component={Home} exact path='/' />
         <Route component={Workshop} path='/:workshop' />
         <NotFound />
       </Switch>
